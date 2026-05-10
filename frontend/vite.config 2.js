8311fc23ace7440977b53a796bed0b7a50ebf399
Srini <srini123@gmail.com>
2026-05-10 13:47:24 +0530

    Fixed Render deployment

--- frontend/vite.config 2.js
+++ frontend/vite.config 2.js
@@ -13,6 +13,6 @@ export default defineConfig({
   preview: {
     host: '0.0.0.0',
     port: process.env.PORT || 4174,
-    allowedHosts: ['campussync-6.onrender.com']
+    allowedHosts:true,
   }
 })
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,3 +1,4 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -13,6 +14,6 @@ export default defineConfig({
   preview: {
     host: '0.0.0.0',
     port: process.env.PORT || 4173,
-    allowedHosts: ['campussync-6.onrender.com']
+    allowedHosts: true
   }
 })
