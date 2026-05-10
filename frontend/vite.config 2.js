97da267568f11f2f403f2894eb4f0b305b7b4746
Srini <srini123@gmail.com>
2026-05-10 13:39:20 +0530

    Updated both vite configs

--- frontend/vite.config 2.js
+++ frontend/vite.config 2.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
+  plugins: [react(), tailwindcss()],
+
+  server: {
+    host: '0.0.0.0',
+    port: 5174
+  },
+
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 4174,
+    allowedHosts: ['campussync-6.onrender.com']
+  }
 })
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
+  plugins: [react(), tailwindcss()],
+
+  server: {
+    host: '0.0.0.0',
+    port: 5173
+  },
+
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 4173,
+    allowedHosts: ['campussync-6.onrender.com']
+  }
 })
